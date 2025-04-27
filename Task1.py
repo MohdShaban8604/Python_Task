44dfbb924abc5cc7efdab15f68fa741d5eaf0869
Mohd Shaban Ansari <105603402+MohdShaban8604@users.noreply.github.com>
2025-04-28 00:32:29 +0530

    Add files via upload
    Task 1: Perform Basic Mathematical Operations
    Task 2: Create a Personalized Greeting

---
+++ Task1.py
@@ -0,0 +1,21 @@
+
+x = int(input("Enter First Number : "))
+y = int(input("Enter Second Number : "))
+
+print("\n")
+
+add = x+y
+5
+print("Addition :", add)
+
+sub = x-y
+
+print("Subtraction :",sub)
+
+mul = x*y
+
+print("Multiplication :", mul)
+
+div = x/y
+
+print("Division :", div)
---
+++ Task2.py
@@ -0,0 +1,7 @@
+frt = input("Enter the first name : ")
+sed = input("Enter the second name : ")
+
+full_name = frt+" "+sed
+BLUE = '\033[94m'   # This makes the text blue
+RESET = '\033[0m'   # This resets the text color back to normal
+print("Hello,",full_name+"! Wellcome",BLUE+"to"+RESET,"the Python program" )
